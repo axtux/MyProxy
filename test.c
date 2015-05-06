aff24c1137fa7cb56afc7699b4b6bba2879db8c1
Axtux <axtux@hotmail.com>
2015-05-06 18:14:40 +0200

    Removed test.c

--- test.c
+++
@@ -1,7 +0,0 @@
-#include "functions.h"
-
-void main(int argc, char** argv) {
-  char *request = "GET / HTTP/1.1\r\nHost: www.google.be\r\nConnection: close\r\n\r\n";
-  printf(get_host(request));
-}
-
