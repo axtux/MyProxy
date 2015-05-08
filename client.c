b8f81c4c56c0d650250d5aa7fccb3759b52fa5cd
Axtux <axtux@hotmail.com>
2015-05-08 16:48:50 +0200

    requested client

--- client.c
+++ client.c
@@ -7,73 +7,55 @@
 #include<arpa/inet.h>
 #include<netinet/in.h>
 #include<unistd.h>
+
 #include "network.h"
 #include "functions.h"
 
-#define SIZEBUF 256 //taille buffer
-
-void main(int argc, char *argv[]){
-  int port;
-  if(argc < 2) {
-    printf("Missing port number. Usage : %s server port\n", argv[0]);
+int main(int argc, char *argv[]){
+  char request[BUFF_SIZE+1], buffer[BUFF_SIZE+1];
+  int port, request_size = 0, buffer_size = 0;
+  
+  if(argc < 3) {
+    printf("Not enough arguments. Usage : %s server port [persistance [pipelining]]\n", argv[0]);
     return -1;
   } else {
-    port = get_int(argv[1], MIN_PORT-1);
+    port = get_int(argv[2], MIN_PORT-1);
     if(port < MIN_PORT || port > MAX_PORT) {
       printf("Invalid port number. Port must range from %d to %d.\n", MIN_PORT, MAX_PORT);
       return -2;
     }
+  
+    sprintf(request, "GET /toi HTTP/1.1\r\nHost: %s\r\n%s\r\n", argv[1], (argc >= 4 && argv[3][0] == '1') ? "" : "Connection: close\r\n");
+    request_size = strlen(request);
+    printf("Made request:%s", request);
   }
   
-  server = conn_socket(host, HTTP_PORT);
+  int server = conn_socket("127.0.0.1", port);
   if(server < 0) {
-    printf("502 Bad Gateway\r\nError connecting to %s.\n", host);
+    printf("Error connecting to proxy server.\n");
   } else {
-    printf("Connected to %s successfully.\n", host);
+    printf("Connected to proxy server successfully.\n");
     if(send(server, request, request_size, 0) != request_size) {
-      printf("Error sending data to server.");
+      printf("Error sending data to server.\n");
     } else {
       do {
         buffer_size = recv(server, buffer, BUFF_SIZE, 0);
         if(buffer_size < 0) {
-          printf("Error receiving data from server.");
+          printf("Error receiving data from server.\n");
+        } else if(buffer_size == 0) {
+          printf("Connection closed by server.\n");
         } else {
-          if(send(client, buffer, buffer_size, 0) != buffer_size) {
-            printf("Error sending data back to the client.");
-          } else {
-          
-          }
+          buffer[buffer_size] = '\0';
+          printf("Data received :%s", buffer);
         }
       } while(buffer_size > 0);
     }
     close(server);
   }
-        
+  /*
   int result;
   char message[SIZEBUF];
-  char buffer[SIZEBUF];
-  
-  //recv(adrFamille, buffer, SIZEBUF, 0);
-  while(1){
-    //send
-    int resultmes = 0;
-    resultmes = scanf("%s",message);
-    int ssize = send(adrFamille, message, SIZEBUF, 0);
-    //printf("%s\n",message);
-    if(ssize ==0 ){
-      printf("problem send cote client\n");
-    
-      close(adrFamille);
-      exit(0);
-    }
-    int nbrOctet = recv(adrFamille, buffer, SIZEBUF, 0);
-    //printf("%s\n",buffer);
-    puts(buffer);
-    memset(buffer, '\0', SIZEBUF);
-    
-  }
-  //close
-  result = close(adrFamille);
-  printf("client fini\n");
-  exit(0);
+  result = scanf("%s",message);
+  //*/
+  return 0;
 }
