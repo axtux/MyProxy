cb44b1531819973aecd32f1190599a72c816e9b8
Axtux <axtux@hotmail.com>
2015-05-10 02:36:58 +0200

    Implements last functionnalities

--- main.c
+++ main.c
@@ -12,8 +12,7 @@
 #include <netinet/in.h>
 #include <netdb.h> 
 
-//int get_int(char string[], int error);
-void connection_handler(void *);
+int connection_handler(void *);
 
 int main(int argc, char* argv[]) {
   int port;
@@ -59,24 +58,20 @@ int main(int argc, char* argv[]) {
 }
 
 
-void connection_handler(void *socket) {
-  int client = *(int*)socket;
+int connection_handler(void *socket) {
+  int close_client = 0, client = *(int*)socket;
   int server = -1;
   
-  char request[BUFF_SIZE], requests[BUFF_SIZE], buffer[BUFF_SIZE];
-  int request_size = 0, requests_size = 0, buffer_size = 0;
+  char request[BUFF_SIZE], response[BUFF_SIZE], requests[BUFF_SIZE], buffer[BUFF_SIZE];
+  int request_size = 0, response_size = 0, requests_size = 0, buffer_size = 0;
   
-  char *host = 0, *uri = 0;
+  char *host = 0, *uri = 0, *cache_file = 0;
   
-  do {
-    buffer_size = recv(client, buffer, BUFF_SIZE, 0);
+  buffer_size = recv(client, buffer, BUFF_SIZE, 0);
+  while(buffer_size > 0) {
     // copy buffered data to requests
     if(requests_size+buffer_size > BUFF_SIZE) {
-      sprintf(buffer, "413 Request Entity Too Large (max %d chars)\r\n\r\n", BUFF_SIZE);
-      send(client, buffer, strlen(buffer), 0);
-      printf(buffer);
-      close(client);
-      return;
+      return http_header(client, "413 Request Entity Too Large", 1);
     }
     memcpy(&requests[requests_size], buffer, buffer_size);
     requests_size += buffer_size;
@@ -84,42 +79,87 @@ void connection_handler(void *socket) {
     while(extract_first_request(requests, &requests_size, request, &request_size) == 0) {
       host = extract_host(request, request_size);
       uri = extract_uri(request, request_size);
-      //host = regmatch(requests, "\r\nhost *: *([\\.A-Za-z]+)\r\n");
+      close_client = mempos(request, "Connection: close\r\n", request_size) +1;
+      
       if(host && uri) {
         server = conn_socket(host, HTTP_PORT);
         if(server < 0) {
-          printf("502 Bad Gateway\r\nError connecting to %s.\n", host);
+          http_header(client, "502 Bad Gateway (1)", close_client);
         } else {
           printf("Connected to %s successfully.\n", host);
+          
+          cache_file = cache_filename(host, uri);
+          remove_header(request, &request_size, "If-Modified-Since");
+          remove_header(request, &request_size, "Connection");
+          add_cache_header(request, &request_size, BUFF_SIZE, http_time(cache_file));
+          add_close_header(request, &request_size, BUFF_SIZE);
+          /* print new request
+          request[request_size] = 0;
+          printf("--- REQUEST ---\n%s", request);
+          //*/
           if(send(server, request, request_size, 0) != request_size) {
-            printf("Error sending data to server.");
+            http_header(client, "502 Bad Gateway (2)", close_client);
           } else {
-            do {
-              buffer_size = recv(server, buffer, BUFF_SIZE, 0);
-              if(buffer_size < 0) {
-                printf("Error receiving data from server.\n");
-              } else if(buffer_size == 0) {
-                printf("Connection closed by server.\n");
-              } else {
-                if(send(client, buffer, buffer_size, 0) != buffer_size) {
-                  printf("Error sending data back to the client.\n");
-                } else {
-                  printf("Data sent back to the client.\n");
-                }
+            response_size = 0;
+            while( (buffer_size = recv(server, buffer, BUFF_SIZE, 0)) > 0) {
+              if(response_size+buffer_size > BUFF_SIZE) {
+                response_size = buffer_size = 0;
+                break;
               }
-            } while(buffer_size > 0);
+              memcpy(&response[response_size], buffer, buffer_size);
+              response_size += buffer_size;
+            }
+            if(buffer_size < 0) {
+              printf("Error receiving data from server.\n");
+              http_header(client, "502 Bad Gateway (3)", close_client);
+            } else { // buffer_size == 0
+              printf("Connection closed by server.\n");
+              if(response_size > 0) {
+                switch(extract_http_code(response, response_size)) {
+                  case 304: // Not Modified / Try to get file from cache
+                    buffer_size = file_get_contents(cache_file, buffer, BUFF_SIZE);
+                    if(buffer_size > 0) {
+                      printf("Got 304, sending data from cache file %s.\n", cache_file);
+                      memcpy(response, buffer, buffer_size);
+                      response_size = buffer_size;
+                    } else {
+                      printf("Got 304 but no cache file %s.\n", cache_file);
+                    }
+                  case 200: // OK / write to cache
+                    file_put_contents(cache_file, response, response_size);
+                  default:
+                    /*
+                    response[response_size] = 0;
+                    printf("--- RESPONSE ---\n%s", response);
+                    //*/
+                    if(send(client, response, response_size, 0) != response_size) {
+                      printf("Error sending data back to the client.\n");
+                      close(client);
+                    } else {
+                      printf("Data sent back to the client.\n");
+                      if(close_client) {
+                        printf("Closing client connection.\n");
+                        close(client);
+                      }
+                    }
+                }
+              } else { // response_size == 0
+                http_header(client, "502 Bad Gateway (4)", close_client);
+              }
+            }
           }
           close(server);
         }
         free(host);
+        free(uri);
       } else {
-        return http_code(client, 400);
+        return http_header(client, "400 Bad Request", 1);
       }
     }
-    
-  } while(buffer_size >= 0); // persistant connection
+    buffer_size = recv(client, buffer, BUFF_SIZE, 0);
+  }
   
-  printf("Connection closed by cient (buffer_size=%d)\n", buffer_size);
+  printf("Connection with client closed\n");
   close(client);
 }
 
